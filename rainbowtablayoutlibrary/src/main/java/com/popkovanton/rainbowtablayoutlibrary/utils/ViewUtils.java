698e6900f7f4ca1e556c9fafc679e20b7b35a0d6
Anton Popkov <popkow.anton@gmail.com>
2020-02-19 08:14:11 +0700

    blend colors with alpha chanel (hotfix)

--- rainbowtablayoutlibrary/src/main/java/com/popkovanton/rainbowtablayoutlibrary/utils/ViewUtils.java
+++ rainbowtablayoutlibrary/src/main/java/com/popkovanton/rainbowtablayoutlibrary/utils/ViewUtils.java
@@ -22,10 +22,11 @@ public class ViewUtils {
      */
     public static int blendColors(int color1, int color2, float ratio) {
         final float inverseRation = 1f - ratio;
+        float alpha = (Color.alpha(color1) * ratio) + (Color.alpha(color2) * inverseRation);
         float r = (Color.red(color1) * ratio) + (Color.red(color2) * inverseRation);
         float g = (Color.green(color1) * ratio) + (Color.green(color2) * inverseRation);
         float b = (Color.blue(color1) * ratio) + (Color.blue(color2) * inverseRation);
-        return Color.rgb((int) r, (int) g, (int) b);
+        return Color.argb((int) alpha, (int) r, (int) g, (int) b);
     }
 
     /**
